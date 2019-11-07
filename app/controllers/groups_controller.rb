2bbc635353cb587276c993fb91dcfaec5abad3a1
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-07 20:39:27 +0900

    group creation function
    グループ作成機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,20 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
 
+  def create
+    @groupe = Groupe.new(groupe_params)
+    if @group.save
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
+  end
 
-
-
+  private
+  def groupe_params
+    params.require(:group).permit(:name, user_ids: [] )
+  end
 end
