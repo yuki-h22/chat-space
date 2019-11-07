dc79c45c00548291d1418cdc392fccd48d68c346
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-07 15:02:52 +0900

    user setting
    user管理のsetting

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
+
+
+#class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+#  devise :database_authenticatable, :registerable,
+#         :recoverable, :rememberable, :trackable, :validatable
+#  validates :nickname, presence: true, length: { maximum: 6 }
+#  has_many :tweets
+#  has_many :comments
+#end
