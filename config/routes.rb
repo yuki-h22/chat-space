6daf4b00071080b06e08ad38398316652ed48b7a
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-08 17:46:49 +0900

    routes　設定
    ルーティングの設定
    問２まで

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:inex, :new, :create, :edit, :update]
+    resources :messages, only:[:index, :create]
 end
