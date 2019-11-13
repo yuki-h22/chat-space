20f4c54e672a621b57fba25ffeb9d395d342997b
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-13 17:11:23 +0900

    Update message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,23 @@
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(post){
+      console.log(post);
+    })
+    .fail(function(){
+
+    })
+  })
+});
+
+
