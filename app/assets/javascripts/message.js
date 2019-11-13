b4e5d723d3210b0492cb1588262ccf3bf0ae0938
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-13 17:44:49 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,28 @@
 $(function(){
+
+
+
+  function buildMessage(post){
+    let html = `<div class="main_center_thread">
+                  <div class="main_center_thread_upper-message">
+                    <div class="main_center_thread_upper-message_name">
+                      ${post.user_id}
+                    </div>
+                    <div class="main_center_thread_upper-message_date">
+                      ${post.created_at}
+                    </div>
+                  </div>
+                  <div class="main_center_thread_lower-message">
+                    <p class="lower-message__content">
+                      ${post.contene}
+                    </p>
+                  </div>
+                </div>`
+    return html;
+  }
+
+
+
   $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -12,7 +36,8 @@ $(function(){
       contentType: false
     })
     .done(function(post){
-      console.log(post);
+      var html = buildMessage(post);
+      $('.main_center').append(html);
     })
     .fail(function(){
 
