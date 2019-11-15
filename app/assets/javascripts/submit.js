67875fb39612f7ec8287f09cce603714a95b5592
yuki-h22 <h-y.e-24-olaf@ezweb.ne.jp>
2019-11-15 11:47:08 +0900

    Update submit.js

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,29 +1,72 @@
-{/* <html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>jQueryの練習</title>
-</head>
-<body>
-    <p>jQueryの練習</p>
-    <button>Add!</button>
- */}
-    // <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
-    // <script>
-        $(function() {
-
-            $('button').click(function() {
-                // テキストが"add!!!"で"add"クラスを持つp要素をボタンの直前に追加する。
-                var p = $('<p>').text('add!!!').addClass('add');
-                $(this).before(p);
-            });
-
-            // このように書いてあげれば、うまく動作します！
-            // jQueryオブジェクトの中は、$('body')ではなく$(document)としてもokです。
-            $('body').on('click', '.add' , function() {
-                $(this).remove();
-            });
-
-        });
-    // </script>
-// </body>
-// </html>
+// $(function() {
+//     function addUser(user) {
+//       let html = `
+//         <div class="chat-group-user clearfix">
+//           <p class="chat-group-user__name">${user.name}</p>
+//           <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+//         </div>
+//       `;
+//       $("#user-search-result").append(html);
+//     }
+  
+//     function addNoUser() {
+//       let html = `
+//         <div class="chat-group-user clearfix">
+//           <p class="chat-group-user__name">ユーザーが見つかりません</p>
+//         </div>
+//       `;
+//       $("#user-search-result").append(html);
+//     }
+//     function addDeleteUser(name, id) {
+//       let html = `
+//       <div class="chat-group-user clearfix" id="${id}">
+//         <p class="chat-group-user__name">${name}</p>
+//         <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+//       </div>`;
+//       $(".js-add-user").append(html);
+//     }
+//     function addMember(userId) {
+//       let html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+//       $(`#${userId}`).append(html);
+//     }
+//     $("#user-search-field").on("keyup", function() {
+//       let input = $("#user-search-field").val();
+//       $.ajax({
+//         type: "GET",
+//         url: "/users",
+//         data: { keyword: input },
+//         dataType: "json"
+//       })
+//         .done(function(users) {
+//           $("#user-search-result").empty();
+  
+//           if (users.length !== 0) {
+//             users.forEach(function(user) {
+//               addUser(user);
+//             });
+//           } else if (input.length == 0) {
+//             return false;
+//           } else {
+//             addNoUser();
+//           }
+//         })
+//         .fail(function() {
+//           alert("通信エラーです。ユーザーが表示できません。");
+//         });
+//     });
+//     $(document).on("click", ".chat-group-user__btn--add", function() {
+//       console.log
+//       const userName = $(this).attr("data-user-name");
+//       const userId = $(this).attr("data-user-id");
+//       $(this)
+//         .parent()
+//         .remove();
+//       addDeleteUser(userName, userId);
+//       addMember(userId);
+//     });
+//     $(document).on("click", ".chat-group-user__btn--remove", function() {
+//       $(this)
+//         .parent()
+//         .remove();
+//     });
+//   });
